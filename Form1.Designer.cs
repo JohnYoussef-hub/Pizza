47f34636881ac000bf42b7027adedd824b0e8e59
John <johnishere.4307@gamil.com>
2024-10-04 16:05:10 +0300

    Fix some issues

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -128,7 +128,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmFirstForm";
             this.Text = "Pizza Boy";
             this.Load += new System.EventHandler(this.frmFirstForm_Load);
--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -486,7 +486,9 @@
             this.Controls.Add(this.gbCrustType);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gbSize);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form2";
             this.Text = "Order Now!";
             this.Load += new System.EventHandler(this.Form2_Load);
